327ca709d9f1ad8b7cc31cdfe567b2d24a7df5e6
Nick Blumberg <nblumberg@hugendubel-digital.de>
2019-09-11 18:49:23 +0200

    finished as poc

--- src/components/App.js
+++ src/components/App.js
@@ -3,39 +3,56 @@ import {WeatherCard} from './WeatherCard';
 import './App.css';
 
 export const App = () => {
-        const apiCall = useRef(
-            fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
-                .then(results => {
-                    return results.json();
-                })
-        );
+    const [apiCall, setApiCall] = useState({});
 
-        setInterval(() => {
-            fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
-                .then(results => {
-                    return results.json();
-                })
-                .then(data => {
-                    apiCall.current = data;
-                    console.log(data);
-                });
-        }, 5000);
+    // setInterval(() => {
+    //     fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
+    //         .then(results => {
+    //             return results.json();
+    //         })
+    //         .then(data => {
+    //             apiCall.current = data;
+    //             console.log("data in interval: " + data);
+    //         });
+    // }, 2000);
 
-        // console.log(apiCall);
+    // useEffect(() => {
+    //     clearTimeout(interval);
+    // });
 
-        return (
-            <>
-                <div className="WeatherBox">
-                    <WeatherCard weekday={0}/>
-                    <WeatherCard weekday={1}/>
-                    <WeatherCard weekday={2}/>
-                    <WeatherCard weekday={3}/>
-                    <WeatherCard weekday={4}/>
-                    <WeatherCard weekday={5}/>
-                    <WeatherCard weekday={6}/>
-                    <WeatherCard weekday={7}/>
-                </div>
-            </>
-        )
-    }
-;
+    useEffect(() => {
+        fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
+            .then(results => {
+                return results.json();
+            })
+            .then(data => {
+                setApiCall(data);
+            });
+    }, []);
+
+    // const interval = setTimeout(() => {
+    //     fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
+    //         .then(results => {
+    //             return results.json();
+    //         })
+    //         .then(data => {
+    //             setApiCall(data);
+    //             console.log(data);
+    //         });
+    // }, 60000);
+
+    return (
+        <>
+            <div className="WeatherBox">
+                <WeatherCard data={apiCall.data} weekday={0}/>
+                <WeatherCard data={apiCall.data} weekday={1}/>
+                <WeatherCard data={apiCall.data} weekday={2}/>
+                <WeatherCard data={apiCall.data} weekday={3}/>
+                <WeatherCard data={apiCall.data} weekday={4}/>
+                <WeatherCard data={apiCall.data} weekday={5}/>
+                <WeatherCard data={apiCall.data} weekday={6}/>
+                <WeatherCard data={apiCall.data} weekday={7}/>
+            </div>
+        </>
+    )
+};
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,16 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "./WeatherCard.css";
-import cloudy1 from "./../resources/cloudy1.png";
-import cloudy2 from "./../resources/cloudy2.png";
-import cloudy3 from "./../resources/cloudy3.png";
-import cloudy4 from "./../resources/cloudy4.png";
-import rainy from "./../resources/rainy.png";
-import snowy from "./../resources/snowy.png";
-import sunny from "./../resources/sunny.png";
-import cloudy_colored from "./../resources/cloudy_colored.png";
-import rainy_colored from "./../resources/rainy_colored.png";
-import snowy_colored from "./../resources/snowy_colored.png";
-import sunny_colored from "./../resources/sunny_colored.png";
 
 export const WeatherCard = (props) => {
     const today = new Date();
@@ -18,31 +7,32 @@ export const WeatherCard = (props) => {
     const weekdays = [
         "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
     ];
-    // const weatherIcons = [
-    //     cloudy1, cloudy2, cloudy3, cloudy4, rainy, snowy, sunny, cloudy_colored, rainy_colored, snowy_colored, sunny_colored
-    // ];
-    // const [counter, setCounter] = useState(0);
-    // const [temperatureDaily, setTemperatureDaily] = useState(NaN);
-    // const [weatherPicDaily, setWeatherPicDaily] = useState({});
-    // const [weatherDescriptionDaily, setWeatherDescriptionDaily] = useState({});
 
-    // setCounter(counter + 1);
-    // setTemperatureDaily(data.data[props.weekday].temp);
-    // setWeatherPicDaily(data.data[props.weekday].weather.icon);
-    // setWeatherDescriptionDaily(data.data[props.weekday].weather.description);
+    // console.log(props.data);
+    // console.log(typeof props.data);
+
+    if (props.data) {
+        return (
+            <div className="WeatherCard">
+                <br/>
+                {day < 7 ? weekdays[day] : weekdays[day - 7]}
+                <br/>
+                <br/>
+                <img
+                    src={"https://www.weatherbit.io/static/img/icons/" + props.data[props.weekday].weather.icon + ".png"}
+                    alt={props.data[props.weekday].weather.description}/>
+                <br/>
+                <br/>
+                {Math.round(props.data[props.weekday].temp) + "\xB0 C"}
+                <br/>
+                <br/>
+            </div>
+        )
+    }
 
     return (
-        <div className="WeatherCard">
-            {day < 7 ? weekdays[day] : weekdays[day - 7]}
-            <br/>
-            <br/>
-            {counter}
-            <br/>
-            <br/>
-            <img src={weatherPicDaily} alt={weatherDescriptionDaily}/>
-            <br/>
-            <br/>
-            {Math.round(temperatureDaily) + "\xB0 C"}
-        </div>
+        <>
+            Loading...
+        </>
     )
 };
