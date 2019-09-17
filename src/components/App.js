52d561efaf6c97524eae18505c9e31e5df24dcd4
Nick Blumberg <nblumberg@hugendubel-digital.de>
2019-09-17 18:38:28 +0200

    deconstructed props and implemented interval fetching api-data --- shrinked code

--- src/components/App.js
+++ src/components/App.js
@@ -1,78 +1,42 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import {WeatherCard} from './WeatherCard';
 import {WeatherCardH} from './WeatherCardH';
-import './App.css';
+
+const fetchWeather = (daily) => fetch(`https://api.weatherbit.io/v2.0/forecast/${
+    daily ? 'daily' : 'hourly'
+}?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3${
+    daily ? '' : '&hours=48'
+}`)
+    .then(results => results.json());
 
 export const App = () => {
-    const [apiCall, setApiCall] = useState({});
-    const [apiCallH, setApiCallH] = useState({});
-
-    // setInterval(() => {
-    //     fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
-    //         .then(results => {
-    //             return results.json();
-    //         })
-    //         .then(data => {
-    //             apiCall.current = data;
-    //             console.log("data in interval: " + data);
-    //         });
-    // }, 2000);
-
-    // useEffect(() => {
-    //     clearTimeout(interval);
-    // });
+    const [apiCall, setApiCall] = useState(null);
+    const [apiCallH, setApiCallH] = useState(null);
 
     useEffect(() => {
-        fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
-            .then(results => {
-                return results.json();
-            })
-            .then(data => {
-                setApiCall(data);
-                console.log("hi");
-            });
-        fetch("https://api.weatherbit.io/v2.0/forecast/hourly?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3&hours=48\n")
-            .then(results => {
-                return results.json();
-            })
-            .then(data => {
-                setApiCallH(data);
-                console.log("derp");
-            });
+        fetchWeather(true).then(setApiCall);
+        setInterval(() => {
+            fetchWeather(true).then(setApiCall);
+        }, 600000);
     }, []);
 
-    // const interval = setTimeout(() => {
-    //     fetch("https://api.weatherbit.io/v2.0/forecast/daily?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3")
-    //         .then(results => {
-    //             return results.json();
-    //         })
-    //         .then(data => {
-    //             setApiCall(data);
-    //             console.log(data);
-    //         });
-    // }, 60000);
-
     const onClick = useCallback(e => {
-        console.log("hello");
-        setApiCallH({
-            d: 1,
-            e: 2
-        });
-        console.log(apiCallH);
-    }, [apiCallH]);
+        fetchWeather(false).then(setApiCallH);
+    }, []);
 
     return (
         <>
-            <div className="WeatherBox">
-                <WeatherCard id={0} data={apiCall.data} hourly={onClick}/>
-                <WeatherCard id={1} data={apiCall.data} hourly={onClick}/>
-                <WeatherCard id={2} data={apiCall.data} hourly={onClick}/>
-                <WeatherCard id={3} data={apiCall.data}/>
-                <WeatherCard id={4} data={apiCall.data}/>
-                <WeatherCard id={5} data={apiCall.data}/>
-                <WeatherCard id={6} data={apiCall.data}/>
-                <WeatherCard id={7} data={apiCall.data}/>
-            </div>
+            {apiCall && (
+                <div className="WeatherBox">
+                    {apiCall.data.map(((data, index) => <WeatherCard key={index} data={data}
+                                                                     hourly={index < 3 ? onClick : null}/>))}
+                </div>
+            )}
+            {apiCallH && (
+                <div className="WeatherBox">
+                    {apiCallH.data.map(((data, index) => <WeatherCardH key={index} data={data}/>))}
+                </div>
+            )}
         </>
     )
 };
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import "./WeatherCard.css";
 
-export const WeatherCard = (props) => {
-    const today = new Date();
-    const day = today.getDay() + props.id;
-    const weekdays = [
-        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
-    ];
-
-    // console.log(props.data);
-    // console.log(typeof props.id);
-
-    if (props.data) {
-        return (
-            <div id={props.id} onClick={props.hourly} className="WeatherCard">
-                <br/>
-                {day < 7 ? weekdays[day] : weekdays[day - 7]}
-                <br/>
-                <br/>
-                <img
-                    src={"https://www.weatherbit.io/static/img/icons/" + props.data[props.id].weather.icon + ".png"}
-                    alt={props.data[props.id].weather.description}/>
-                <br/>
-                <br/>
-                {Math.round(props.data[props.id].temp) + "\xB0 C"}
-                <br/>
-                <br/>
-            </div>
-        )
-    }
-
-    return (
-        <div/>
-    )
-};
+export const WeatherCard = ({id, data: {ts, temp, weather: {description, icon}}, hourly}) => (
+    <div id={id} onClick={hourly} className="WeatherCard">
+        <br/>
+        {new Date(ts * 1000).toDateString().slice(0, 3)}
+        <br/>
+        <br/>
+        <img
+            src={"https://www.weatherbit.io/static/img/icons/" + icon + ".png"}
+            alt={description}/>
+        <br/>
+        <br/>
+        {Math.round(temp) + "\xB0 C"}
+        <br/>
+        <br/>
+    </div>
+);
