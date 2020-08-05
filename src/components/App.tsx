ccf796628f90ed2eb2d1e11bcf5b4234fa51c478
Blumberg1337 <nick.blumberg@outlook.de>
2020-08-06 01:49:14 +0200

    Update deps

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { WeatherCard } from './WeatherCard';
-import { WeatherCardH } from './WeatherCardH';
 import { Staged } from 'react-staged';
-import {
-    WeatherbitResponse,
-    WeatherbitResponseH,
-    WeatherData,
-    WeatherDataH,
-    fetchWeather,
-} from '../services/WeatherService';
+import { fetchWeather, WeatherbitResponse, WeatherData } from '../services/WeatherService';
 
 export const App: React.FC = () => {
+    const [itemsPerPage, setItemsPerPage] = useState<number>(1);
     const [apiCall, setApiCall] = useState<WeatherbitResponse | null>(null);
-    const [apiCallH, setApiCallH] = useState<WeatherbitResponseH | null>(null);
+    // const [apiCallH, setApiCallH] = useState<WeatherbitResponseH | null>(null);
 
     useEffect(() => {
         fetchWeather(true).then(setApiCall);
@@ -21,9 +15,21 @@ export const App: React.FC = () => {
         }, 600000);
     }, []);
 
-    const onClick = () => {
-        fetchWeather(false).then(setApiCallH);
-    };
+    // need new api key... from another api provider like https://openweathermap.org/api
+    // const onClick = () => {
+    //     fetchWeather(false).then(setApiCallH);
+    // };
+
+    useEffect(() => {
+        const windowSize = window.innerWidth;
+        if (windowSize >= 800) {
+            setItemsPerPage(8);
+        } else if (windowSize >= 600) {
+            setItemsPerPage(4);
+        } else if (windowSize >= 300) {
+            setItemsPerPage(2);
+        }
+    }, []);
 
     // const showHours = () => {
     //     if (apiCallH) {
@@ -31,34 +37,34 @@ export const App: React.FC = () => {
     //     }
     // };
 
-    console.log(apiCallH);
+    // console.log(apiCallH);
     console.log(apiCall);
 
     return (
         <>
             {apiCall && (
                 <div className="WeatherBox">
-                    <Staged amount={8}>
+                    <Staged amount={itemsPerPage}>
                         {apiCall.data.map((data: WeatherData, index: number) => (
                             <WeatherCard
                                 key={index}
                                 data={data}
-                                hourly={index < 3 ? onClick : undefined}
+                                // hourly={index < 3 ? onClick : undefined}
                                 id={String(index)}
                             />
                         ))}
                     </Staged>
                 </div>
             )}
-            {apiCallH && (
-                <div className="WeatherBox">
-                    <Staged amount={8}>
-                        {apiCallH.data.map((data: WeatherDataH, index: number) => (
-                            <WeatherCardH key={index} data={data} />
-                        ))}
-                    </Staged>
-                </div>
-            )}
+            {/*{apiCallH && (*/}
+            {/*    <div className="WeatherBox">*/}
+            {/*        <Staged amount={8}>*/}
+            {/*            {apiCallH.data.map((data: WeatherDataH, index: number) => (*/}
+            {/*                <WeatherCardH key={index} data={data} />*/}
+            {/*            ))}*/}
+            {/*        </Staged>*/}
+            {/*    </div>*/}
+            {/*)}*/}
         </>
     );
 };
--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -100,4 +100,4 @@ export const fetchWeather = (daily: boolean) =>
         `https://api.weatherbit.io/v2.0/forecast/${
             daily ? 'daily' : 'hourly'
         }?lang=de&postal_code=22761&country=de&key=4cb12e098d664cf3971b6cd11ea62ea3${daily ? '' : '&hours=48'}`
-    ).then(results => results.json());
+    ).then((results) => results.json());
